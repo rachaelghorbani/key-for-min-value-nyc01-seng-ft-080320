3a5f7a070a15379b41f9e97fbde1ef12bfb3cb51
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-14 00:40:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,24 +1,20 @@
 def key_for_min_value(name_hash)
-if name_hash == {}
-  return nil
-end
+  if name_hash == {}
+    return nil
+  end
 
-array_of_keys = []
-array_of_values = []
+  array_of_values = []
 
-name_hash.each do |key, value|
-   array_of_keys.push(key)
-  array_of_values.push(value)
-end
+  name_hash.each do |key, value|
+    array_of_values.push(value)
+  end
 
-   lowest_number = array_of_values[0]
-   array_of_values.each do |num|
+  lowest_number = array_of_values[0]
+  array_of_values.each do |num|
     if num < lowest_number
       lowest_number = num 
     end
   end
-  
- 
 
   name_hash.each do |key, value|
     if value == lowest_number
